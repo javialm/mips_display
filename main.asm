70a21dd58ef8b71c016aa078ee4a97af9374b6f8
javialm <javialm8@gmail.com>
2018-05-05 00:01:33 +0200

    error handling

--- main.asm
+++ main.asm
@@ -1,8 +1,14 @@
 # Pedir por teclado el tamaño del vector
 .data
+welcome1: .asciiz "########## WELCOME ###########\n"
+welcome2: .asciiz "# Valores permitidos: 0 - 99 #\n"
 string1: .asciiz "Introduce el tamaño del vector:"
 string2: .asciiz "Dime el valor "
+string3: .asciiz "Resultado: "
+string4: .asciiz "Error: Introduce un valor entre 0 y 99"
 tamaño: .byte 0
+min: 	.byte 0x00
+max:	.byte 0x63
 #cero: .byte 0x0A
 #vector: .word
 
@@ -10,16 +16,28 @@ tamaño: .byte 0
 	li $s0, 0xFFFF0010 # carga dirección base del displayderecho
 	li $s1, 0xFFFF0011 # carga dirección base del displayizquierdo
 	
-	li $s3, 0x10010040
+	li $s3, 0x100100c0
+	#========================= WELCOME =================================
+	la 	$a0, welcome1
+	li	$v0, 4
+	syscall
+	
+	la 	$a0, welcome2
+	li	$v0, 4
+	syscall
 	
 	#========================= TAMAÑO DEL VECTOR =========================
 	# Completar el vector con valores por teclado, TIENEN QUE SER NUMEROS
+vector:
 	la 	$a0, string1		# --- "Introduce el tamaño del vector:"
 	li	$v0, 4
 	syscall
 	
 	li	$v0, 5			# leemos un entero introducido por teclado
 	syscall
+	
+	#blt 	$t0, $t1, errors saltar si el dato leido es menor que....
+	
 	move 	$t1, $v0		# t1 tamaño, t0 cont
 	sb	$t1, tamaño
 	.data
@@ -60,7 +78,10 @@ ask:
 	num:
 	#andi	$t0, $t0, 1 # cont++
 	#blt 	$t0, $t1, num
-	move 	$a0, $t0
+	la 	$a0, string3		# --- "Resultado media aritmetica"
+	li	$v0, 4
+	syscall
+	move 	$a0, $t5
 	li	$v0, 1
 	syscall
 	
@@ -76,3 +97,9 @@ ask:
 	
 end:	li	$v0, 10			# Fin del programa#
 	syscall
+	
+	
+errors: la 	$a0, string4		# --- "Resultado media aritmetica"
+	li	$v0, 4
+	syscall
+	j ask
